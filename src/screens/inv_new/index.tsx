b8150c7afca480ecb6bffbfa9205c2d8f3c0eb83
aiseeingvoice <aiseeingvoice@gmail.com>
2025-08-16 08:48:52 -0400

    v

--- src/screens/inv_new/index.tsx
+++ src/screens/inv_new/index.tsx
@@ -3,4 +3,3 @@ export * from '@/src/screens/inv_new/Inv2Client';
 export * from '@/src/screens/inv_new/Inv3Items';    
 export * from '@/src/screens/inv_new/Inv4Total';
 export * from '@/src/screens/inv_new/Inv5Notes';
-export * from '@/src/screens/inv_pay/InvPay';
---
+++ src/screens/inv_pay/index.tsx
@@ -0,0 +1,5 @@
+export * from '@/src/screens/inv_pay/InvPay';
+export * from '@/src/screens/inv_pay/InvPay_Edit';
+export * from '@/src/screens/inv_pay/InvPay_Edit_Client';
+
+
